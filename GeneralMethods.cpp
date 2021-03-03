43ecb596005de03bcd9fc5a05b79e51b719afbd1
bubbajono <jonathan.hunt159@gmail.com>
2021-03-03 23:07:07 +0000

    Optimise IsPrime method

--- GeneralMethods.cpp
+++ GeneralMethods.cpp
@@ -15,7 +15,7 @@ namespace GeneralMethods
             return true;
         }
 
-        for (int i = 2; i < num; i++)
+        for (int i = 2; i < sqrt(num); i++)
         {
             if (num % i == 0)
             {
